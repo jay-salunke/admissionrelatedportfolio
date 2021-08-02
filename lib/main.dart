824abfc6c00b8de459fa2a6a5d6bf9ffd5d11e2e
Jay-Salunke <jysl2004@gmail.com>
2021-08-02 20:21:48 +0530

    refactor the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,31 @@
-import 'package:admission_portfolio/Pages/homepage.dart';
+import 'package:admission_portfolio/Pages/adminPage.dart';
+import 'package:admission_portfolio/Pages/userScreen.dart';
 import 'package:admission_portfolio/Pages/login.dart';
-import 'package:admission_portfolio/Pages/signup.dart';
+import 'package:admission_portfolio/Pages/signUp.dart';
+import 'package:admission_portfolio/authentication.dart';
+import 'package:admission_portfolio/userManagement.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:admission_portfolio/Pages/firstpage.dart';
+import 'package:admission_portfolio/Pages/firstPage.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    // home:FirstPage(),
+     home:FirstPage(),
     initialRoute: FirebaseAuth.instance.currentUser == null?'/firstPage':'/homepage',
     routes: <String,WidgetBuilder>{
       '/firstPage':(BuildContext context)=>FirstPage(),
       '/login':(BuildContext context)=>Login(),
       '/signUp':(BuildContext context)=>SignupPage(),
       '/homepage':(BuildContext context)=>HomePage(),
+      '/adminPage':(BuildContext context)=>AdminPage(),
     },
   ));
 }
+
 
 
