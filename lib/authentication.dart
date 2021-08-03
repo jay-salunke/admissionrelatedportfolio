8571520df707aa2da832b92419b4fd1fe55eb536
Jay-Salunke <jysl2004@gmail.com>
2021-08-03 22:32:21 +0530

    Created the file authentication.dart file and created init function which check user logged in or not and also check user role

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -1,89 +1,57 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-//
-//
-// class Authentication{
-//
-//   String role="";
-//   checkUserRole() async {
-//   String uid = FirebaseAuth.instance.currentUser!.uid.toString();
-//   final userRef = FirebaseFirestore.instance.collection("UsersDetails");
-//   await userRef.where('uid',isEqualTo: uid).get().then((
-//   QuerySnapshot value) => value.docs.forEach((DocumentSnapshot element) {
-//   if((element.data() as Map<String,dynamic>)['Role'] == 'Admin'){
-//   role = "Admin";
-//   }else if((element.data() as Map<String,dynamic>)['Role'] == 'User'){
-//   role = "User";
-//   }else{
-//     role = "Unauthorized";
-//   }
-//   }));
-//   return role;
-//   }
-//
-// }
-import 'package:admission_portfolio/Pages/adminPage.dart';
 import 'package:admission_portfolio/Pages/login.dart';
-import 'package:admission_portfolio/Pages/userScreen.dart';
-import 'package:admission_portfolio/loadingPage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Authentication extends StatefulWidget {
-  const Authentication({Key? key}) : super(key: key);
+class AuthChecker extends StatefulWidget {
+  const AuthChecker({Key? key}) : super(key: key);
 
   @override
-  _AuthenticationState createState() => _AuthenticationState();
+  _AuthCheckerState createState() => _AuthCheckerState();
 }
 
-class _AuthenticationState extends State<Authentication> {
-
+class _AuthCheckerState extends State<AuthChecker> {
   @override
   void initState() {
-
     super.initState();
+
+    if (FirebaseAuth.instance.currentUser != null) {
+      String uid = FirebaseAuth.instance.currentUser!.uid.toString();
+      final userRef = FirebaseFirestore.instance.collection("UsersDetails");
+      userRef.where('uid', isEqualTo: uid).get().then(
+              (QuerySnapshot value) {
+            final element = value.docs[0];
+            if (element.exists) {
+              if ((element.data() as Map<String, dynamic>)['Role'] ==
+                  'Admin') {
+                Navigator.pushNamedAndRemoveUntil(
+                    context, '/adminPage', (Route<dynamic> route) => false);
+              }
+              else if ((element.data() as Map<String, dynamic>)['Role'] ==
+                  'User') {
+                print("User");
+                Navigator.pushNamedAndRemoveUntil(
+                    context, '/homepage', (Route<dynamic> route) => false);
+              }
+            }
+            else
+              Navigator.pushNamedAndRemoveUntil(
+                  context, '/login', (Route<dynamic> route) => false);
+          });
+
+    }else{
+        Login();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (BuildContext context, snapshot){
-           if(snapshot.hasData){
-             if(checkUserRole() == 'Admin')AdminPage();
-             else if(checkUserRole() == 'User')HomePage();
-             else LoaderPage();
-           }
-           if(snapshot.connectionState == ConnectionState.waiting){
-             return LoaderPage();
-           }
-           return Login();
-        }
-
-    );
+    return Container();
   }
 }
 
-
-  checkUserRole() async {
-   String role ="";
-  String uid = FirebaseAuth.instance.currentUser!.uid.toString();
-  final userRef = FirebaseFirestore.instance.collection("UsersDetails");
-  await userRef.where('uid',isEqualTo: uid).get().then((
-  QuerySnapshot value) => value.docs.forEach((DocumentSnapshot element) {
-  if((element.data() as Map<String,dynamic>)['Role'] == 'Admin'){
-  role = "Admin";
-  }else if((element.data() as Map<String,dynamic>)['Role'] == 'User'){
-  role = "User";
-  }else{
-    role = "Unauthorized";
-  }
-  }));
-  return role;
-  }
-
-
-
-
-
+  // @override
+  // Widget build(BuildContext context) {
+  //   // TODO: implement build
+  //   throw UnimplementedError();
+  // }
